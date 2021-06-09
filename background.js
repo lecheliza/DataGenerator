3c9708bf6ee92ba8ad7f5299cea2c7dc36e70470
lecheliza <lechoosz@gmail.com>
2021-06-09 10:29:15 +0200

    Data can be saved in the local storage.

--- background.js
+++ background.js
@@ -4,12 +4,13 @@ function generateString() {
     var pass = [];
     var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+
     let index;
     let outdex = 0;
     var elements = document.getElementsByTagName("input");
     for (index = 0; index < elements.length; index++) {
         result = [];
-        for (outdex = 0; outdex < 10; outdex++) {
+        for (outdex = 0; outdex < 12; outdex++) {
             result.push(characters.charAt(Math.floor(Math.random() * characters.length)));
         }
         if (elements[index].type === "password") {
@@ -19,8 +20,14 @@ function generateString() {
         if (elements[index].type === "password") {
             elements[index].value = pass.join('');
         }
+        window.localStorage.setItem(elements[index].name, elements[index].value);
+    }
+    if (window.localStorage.length !== 0) alert("went right");
+    for (index = 0; index < window.localStorage.length; index++) {
+        console.log(window.localStorage.getItem(elements[index].name));
     }
 }
+
 
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
@@ -28,3 +35,7 @@ chrome.action.onClicked.addListener((tab) => {
         function: generateString
     });
 });
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, document.getElementsByTagName("input"));
+// });
