b59d8c47485441b2ef91201dcefe5a011b3cb0c7
lecheliza <lechoosz@gmail.com>
2021-06-11 15:47:51 +0200

    Phone number included

--- background.js
+++ background.js
@@ -3,27 +3,51 @@ function generateString() {
     var pass = [];
     var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+    var specialCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890;!@()[]{}#?';
+    var numbers = '0123456789';
 
     let index;
     let outdex = 0;
     var elements = document.getElementsByTagName("input");
     for (index = 0; index < elements.length; index++) {
         result = [];
-        for (outdex = 0; outdex < 12; outdex++) {
-            result.push(characters.charAt(Math.floor(Math.random() * characters.length)));
-        }
-        if (elements[index].type === "password") {
-            if (pass.length === 0) pass.push(result.join(''));
-        }
-        elements[index].value = result.join('');
-        if (elements[index].type === "password") {
-            elements[index].value = pass.join('');
+        switch (elements[index].type) {
+            case "password":
+                if (pass.length === 0) {
+                    for (outdex = 0; outdex < 15; outdex++) {
+                        pass.push(specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length)));
+                    }
+                }
+                elements[index].value = pass.join('');
+                break;
+
+            case "tel":
+                for (outdex = 0; outdex < 9; outdex++) {
+                    result.push(numbers.charAt(Math.floor(Math.random() * numbers.length)));
+                }
+                elements[index].value = result.join('');
+                break;
+
+            case "text":
+                for (outdex = 0; outdex < 12; outdex++) {
+                    result.push(alphabet.charAt(Math.floor(Math.random() * alphabet.length)));
+                }
+                elements[index].value = result.join('');
+                break;
+
+            default:
+                for (outdex = 0; outdex < 12; outdex++) {
+                    result.push(characters.charAt(Math.floor(Math.random() * characters.length)));
+                }
+                elements[index].value = result.join('');
+                break;
         }
     }
+
         if (confirm("Do you want to save data?")) {
             for (let index = 0; index < elements.length; index++) {
                 window.localStorage.setItem(elements[index].name, elements[index].value);
-                console.log(window.localStorage.getItem(elements[index].name));
+                console.log(elements[index].name + " " + window.localStorage.getItem(elements[index].name));
             }
         }
 }
